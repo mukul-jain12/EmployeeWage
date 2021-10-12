9086eba116c11fdf932d484e4ebe1c7089d83828
Mukul Jain <mukuljain422@gmail.com>
2021-10-12 18:52:08 +0530

    check attendance

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,1 +1,19 @@
 //EmpLoyee Wage Builder
+
+public class EmpWageBuilder {
+
+	public static void main(String args[]) {
+
+		//constant
+		int is_full_time = 1;
+
+		//Generate random value to check employee present or absent
+		double empcheck = Math.floor(Math.random()*10) % 2;
+
+		//Condition statement
+		if(is_full_time == empcheck)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+	}
+}
