db6c84885c611c88b54c63b25d630a56821346db
Mukul Jain <mukuljain422@gmail.com>
2021-10-12 19:07:15 +0530

    also calculate wage for part time

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -6,17 +6,21 @@ public class EmpWageBuilder {
 
 		//constant
 		int is_full_time = 1;
+		int is_part_time = 2;
 		int empRatePerHr = 20;
 
+		//variables
 		int empHours = 0;
 		int empWage = 0;
 
-		//Generate random value to check employee present or absent
-		double empcheck = Math.floor(Math.random()*10) % 2;
+		//Generate random value to check employee do work full time, part time or absent
+		double empcheck = Math.floor(Math.random()*10) % 3;
 
 		//Condition statement
 		if(is_full_time == empcheck)
 			empHours = 8;
+		else if(is_part_time == empcheck)
+			empHours = 4;
 		else
 			empHours = 0;
 		
