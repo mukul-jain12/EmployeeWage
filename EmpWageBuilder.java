78adfb272393241a6b0204fefa2ef27bdf6edbf7
Mukul Jain <mukuljain422@gmail.com>
2021-10-12 19:33:26 +0530

    monthly wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,34 +1,41 @@
 //EmpLoyee Wage Builder
 
 public class EmpWageBuilder {
+	//constant
+	public static final int is_full_time = 1;
+	public static final int is_part_time = 2;
+	public static final int empRatePerHr = 20;
+	public static final int numOfWorkingday = 2;
 
+	//main method
 	public static void main(String args[]) {
-
-		//constant
-		int is_full_time = 1;
-		int is_part_time = 2;
-		int empRatePerHr = 20;
 
 		//variables
 		int empHours = 0;
 		int empWage = 0;
+		int totalEmpWage = 0;
 
-		//Generate random value to check employee do work full time, part time or absent
-		double empcheck = Math.floor(Math.random()*10) % 3;
+		for (int i = 0; i < numOfWorkingDay; i++){
+			//Generate random value to check employee do work full time, part time or absent
+			double empcheck = Math.floor(Math.random()*10) % 3;
 
-		//Condition statement switch case
-		switch(empcheck){
-			case is_full_time:
-				empHours = 8;
-				break;
-			case is_part_time
-				empHours = 4;
-				break;
-			default:
-				empHours = 0;
+			//Condition statement switch case
+			switch(empcheck){
+				case is_full_time:
+					empHours = 8;
+					break;
+				case is_part_time
+					empHours = 4;
+					break;
+				default:
+					empHours = 0;
+			}
+
+			//Calculat employee wage
+			empWage = empHours * empRatePerHr;
+			System.out,println("Employee Wage : " + empWage);
+			totalEmpWage += empWage;
 		}
-		//Calculat employee wage
-		empWage = empHours * empRatePerHr;
-		System.out,println("Employee Wage : " + empWage);
+		System.out.println("Monthly Wage Of Employee : " + totalEmpWage);
 	}
 }
